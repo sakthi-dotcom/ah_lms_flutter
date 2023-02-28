bfa428adb6a7be31a63e95ec068198d0ba9e39d5
Sakthivel K <sakthivel.karunanithi@augustahitech.com>
2023-02-28 19:04:37 +0530

    Added appbar and drawer option in Dashboard screen.

--- lib/dashboard.dart
+++ lib/dashboard.dart
@@ -1,5 +1,6 @@
+import "package:ah_lms/constant.dart";
 import "package:flutter/material.dart";
-
+import 'sidebar.dart';
 
 class Dashboard extends StatefulWidget {
   const Dashboard({Key? key}) : super(key: key);
@@ -11,6 +12,13 @@ class Dashboard extends StatefulWidget {
 class _DashboardState extends State<Dashboard> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      drawer: const SideBar(),
+      appBar: AppBar(
+        title: const Text(
+          appName
+        ),
+      ),
+    );
   }
 }
