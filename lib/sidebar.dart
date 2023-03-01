ac4ba9c3c27b88e26f64ce097afaf16de769ba63
Sakthivel K <sakthivel.karunanithi@augustahitech.com>
2023-03-01 19:00:25 +0530

    updated drawer header.

--- lib/sidebar.dart
+++ lib/sidebar.dart
@@ -23,27 +23,55 @@ class _SideBarState extends State<SideBar> {
       child: ListView(
         padding: EdgeInsets.zero,
         children: [
-          UserAccountsDrawerHeader(
-            accountName: Text("Sakthivel K"),
-            accountEmail: Text("sakthivel.karunanithi@augustahitech.com"),
-            currentAccountPicture: CircleAvatar(
-              child: ClipOval(
-                  child: Image.asset(
-                "assets/avatar.png",
-                width: 100.0,
-                height: 100.0,
-                fit: BoxFit.cover,
-              )),
+          // UserAccountsDrawerHeader(
+          //   accountName: Text("Sakthivel K"),
+          //   accountEmail: Text("sakthivel.karunanithi@augustahitech.com"),
+          //   currentAccountPicture: CircleAvatar(
+          //     child: ClipOval(
+          //         child: Image.asset(
+          //       "assets/avatar.png",
+          //       width: 100.0,
+          //       height: 100.0,
+          //       fit: BoxFit.cover,
+          //     )),
+          //   ),
+          // ),
+          Container(
+            height: 350.0,
+            child: DrawerHeader(
+              decoration: const BoxDecoration(color: Colors.white),
+              child: Column(
+                children: const [
+                  Image(
+                    image: AssetImage("assets/title.png"),
+                  ),
+                  SizedBox(
+                    height: 25.0,
+                  ),
+                  CircleAvatar(radius: 80.0,),
+                  SizedBox(
+                    height: 10.0,
+                  ),
+                  Center(
+                    child: Text(
+                      "Sakthivel K",
+                      style: TextStyle(
+                        color: Colors.grey
+                      ),
+                    ),
+                  )
+                ],
+              ),
             ),
           ),
-           ListTile(
+          ListTile(
             leading: const Icon(Icons.send),
-            title: const Text(app_leave,),
-            onTap: (){
-              Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => ApplyLeave() )
-              );
+            title: const Text(
+              app_leave,
+            ),
+            onTap: () {
+              Navigator.push(context,
+                  MaterialPageRoute(builder: (context) => ApplyLeave()));
             },
           ),
           ListTile(
@@ -51,39 +79,31 @@ class _SideBarState extends State<SideBar> {
             title: const Text(my_leave),
             onTap: () {
               Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => MyLeave() )
-              );
+                  context, MaterialPageRoute(builder: (context) => MyLeave()));
             },
           ),
           ListTile(
             leading: const Icon(Icons.request_page),
             title: const Text(lev_req),
             onTap: () {
-              Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => LeaveRequest() )
-              );
+              Navigator.push(context,
+                  MaterialPageRoute(builder: (context) => LeaveRequest()));
             },
           ),
           ListTile(
             leading: const Icon(Icons.group),
             title: const Text(Aug_fam),
             onTap: () {
-              Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => AugustaFamily() )
-              );
+              Navigator.push(context,
+                  MaterialPageRoute(builder: (context) => AugustaFamily()));
             },
           ),
           ListTile(
             leading: const Icon(Icons.holiday_village),
             title: const Text(holidays),
             onTap: () {
-              Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => AugustaHolidays() )
-              );
+              Navigator.push(context,
+                  MaterialPageRoute(builder: (context) => AugustaHolidays()));
             },
           ),
           ListTile(
@@ -91,36 +111,35 @@ class _SideBarState extends State<SideBar> {
             title: const Text(profile),
             onTap: () {
               Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => Profile() )
-              );
+                  context, MaterialPageRoute(builder: (context) => Profile()));
             },
           ),
           ListTile(
             leading: const Icon(Icons.key),
-            title: const Text(chg_password,),
+            title: const Text(
+              chg_password,
+            ),
             onTap: () {
-              Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => ChangePassword() )
-              );
+              Navigator.push(context,
+                  MaterialPageRoute(builder: (context) => ChangePassword()));
             },
           ),
           ListTile(
-            leading: const Icon(Icons.logout,color: Colors.red,),
-            title: const Text(logout,style: TextStyle(
-              color: Colors.red
-            ),),
-            onTap: (){
+            leading: const Icon(
+              Icons.logout,
+              color: Colors.red,
+            ),
+            title: const Text(
+              logout,
+              style: TextStyle(color: Colors.red),
+            ),
+            onTap: () {
               Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => Logout() )
-              );
+                  context, MaterialPageRoute(builder: (context) => Logout()));
             },
           ),
         ],
       ),
     );
   }
-
 }
