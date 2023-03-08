3e13186b80874b1d87131034b77f09c2a744c635
Sakthivel K <sakthivel.karunanithi@augustahitech.com>
2023-03-08 14:08:19 +0530

    removed debug banner

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,7 @@ class InitialScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return  MaterialApp(
       home: LoginScreen(),
+      debugShowCheckedModeBanner: false,
       theme:ThemeData(
         primaryColor: Colors.blueAccent,
       ),
