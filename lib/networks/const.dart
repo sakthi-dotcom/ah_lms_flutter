b4def1159a621d37440167f0265784791425c51b
Sakthivel K <sakthivel.karunanithi@augustahitech.com>
2023-03-08 18:38:49 +0530

    started to work product API

--- lib/networks/const.dart
+++ lib/networks/const.dart
@@ -3,3 +3,8 @@ class ApiConstants{
   static String baseUrl = 'https://dummyjson.com/';
   static String productEndpoint = 'products';
 }
+
+class productApiCons{
+  static String productBaseUrl = "https://dummyjson.com/";
+  static String productEndpoint = "products";
+}
